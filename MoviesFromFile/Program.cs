5209322b30b624b8e42c6b59c402659698b059a8
Opilane TTHK <opilane@tthk.ee>
2021-12-02 10:30:12 +0200

    Week14-Frozen

---
+++ MoviesFromFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+namespace MoviesFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static string[] getMoviesFromfile()
+        {
+            string FilePath = @"C:\Users\opilane\samples\LOGITpe21\Movies.txt";
+            string[] dataFromFile = File.ReadAllLines(FilePath);
+
+            return dataFromFile;
+
+        }
+        public static void ShowDataFromArray(string[] someArray)
+        {
+          foreach (string line in someArray)
+          {        
+             Console.WriteLine(line);
+          }
+           
+        }   
+    
+    }
+
+}        
+    
+
